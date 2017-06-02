bd550782634fe40608effd07cec262b6556ef83c
Eric Pires <ericpires9@gmail.com>
2017-06-02 17:08:00 -0300

    Added example assembly

---
+++ src/item-2-2.s
@@ -0,0 +1,12 @@
+	.text
+	.globl	main
+main:
+	MOV	r0, #15                 
+	MOV	r1, #20
+	BL	firstfunc               
+	MOV	r0, #0x18               
+	LDR	r1, =0x20026            
+	SWI	0x123456                
+firstfunc:
+	ADD	r0, r0, r1              
+	MOV	pc, lr  
