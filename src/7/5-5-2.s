7371eb13ff741af6bba535a443d7c1f07aeb9a03
Rodrigo Masaru Ohashi <ohashi.masaru@gmail.com>
2018-06-15 14:11:57 -0300

    Refactor part 7 exercises
    Add commentaries and fix the logic for the routine calls

--- src/7/5-5-2.s
+++ src/7/5-5-2.s
@@ -7,11 +7,11 @@ termina:
   LDR   r0, =0x20026
   SWI   0x123456
 factorial:
-  MOV   r6, #0xA
+  MOV   r6, #0xA        /* n */
   MOV   r4, r6
 loop:
   SUBS  r4, r4, #1
-  MULNE r1, r6, r4
-  MOVNE r6, r1
+  MULNE r1, r6, r4      /* (n x n - 1) */
+  MOVNE r6, r1          /* resultado em r6 */
   BNE loop
   MOV pc, lr
